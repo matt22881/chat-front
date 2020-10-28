d3966dd85c5aec48eefe095056aefff2e3526271
matt22881 <matt22881@gmail.com>
2020-10-28 14:09:47 -0400

    it might work

---
+++ scripts.js
@@ -0,0 +1,49 @@
+// const io = require('socket.io-client')
+
+// const port = process.env.PORT || 5555
+
+// const socket = io('http://localhost:5555')
+const socket = io.connect('https://chatty-matty.herokuapp.com/');
+
+const getLocalTime = () => {
+    const d = new Date()
+    var t = d.toLocaleString()
+    return t
+}
+
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-form')
+const messageInput = document.getElementById('message-input')
+
+let name = prompt('enter a username')
+appendMessage(`You joined as ${name}`, 'user')
+socket.emit('new-user', name)
+   
+socket.on('chat-message', (data) => {
+    appendMessage(`${data.name}: ${data.message}`, data.style) 
+})
+
+socket.on('user-connected', (name, style) => {
+    appendMessage(`${name} connected`, style)
+})
+
+socket.on('user-disconnected', (name, style) => {
+    appendMessage(`${name} disconnected`, style)
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    socket.emit('send-chat-message', message)
+    appendMessage(`You: ${message}`, 'user')
+    messageInput.value = ''
+})
+
+function appendMessage(message, style) {
+    const t = getLocalTime()
+    const messageElement = document.createElement('div')
+    messageElement.classList.add(style)
+    messageElement.classList.add('message')
+    messageElement.innerText = `[${t}] ${message}`
+    messageContainer.insertBefore(messageElement, messageContainer.firstChild)
+}
